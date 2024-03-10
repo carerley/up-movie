8419d9c130284d6b16117b820f04aa876c52f653
Alex Li <carer@Alexs-iMac.local>
2024-03-10 12:12:12 -0400

    selector

---
+++ crawler.py
@@ -0,0 +1,12 @@
+class MovieFinder:
+    TITLE_SELECTOR = ".PosterContent a h3"
+
+    def __init__(self, start_url: str) -> None:
+        pass
+
+
+    def findAllTitles() -> list[str]:
+        pass
+
+
+# if __name__ == "__main__":
