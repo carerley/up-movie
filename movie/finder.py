5566cf689e2324bebd47baafc0776958bec76808
Alex Li <carerley@gmail.com>
2024-10-21 23:51:42 -0400

    selenium find all details

--- movie/finder.py
+++ movie/finder.py
@@ -1,20 +1,32 @@
 import os
+import sys
+from typing import List
 
 from lxml import html
-from selenium.webdriver.chrome import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.options import ArgOptions
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 from movie.requestor import Requestor
 from movie.web_requestor import WebRequestor, ZenRowWebRequestor
 
 AMC_TITLE_SELECTOR = ".PosterContent a h3"
+AMC_DETAILS_PAGE_LINK_SELECTOR = ".PosterContent a:first-child"
+AMC_DETAILS_PAGE_TITLE_SELECTOR = "h1.headline-paused"
 AMC_DOMAIN = "www.amctheatres.com"
 AMC_INTERNATIONAL_FILM_URL = f"https://{AMC_DOMAIN}/programs/international-films"
 WEB_DRIVER_PATH = os.getenv("WEB_DRIVER_PATH")
 
 
 class TitleFinder:
+    def __init__(self) -> None:
+        sys.path.append(WEB_DRIVER_PATH)
+        self.driver = webdriver.Chrome()
+
     @classmethod
     def find_all_titles(cls, base_url: str, title_selector: str) -> list[str]:
         """
@@ -34,18 +46,32 @@ class TitleFinder:
         """
         return cls.__find_all_titles(ZenRowWebRequestor(), base_url, title_selector)
 
-    @classmethod
-    def selenium_find_all_titles(cls, base_url: str, title_selector: str) -> list[str]:
-        """use selenium to drive a browser to scrape element like human. """
-        webdriver.Options
-        driver = webdriver.ChromiumDriver(options={"driver_path": WEB_DRIVER_PATH})
-        driver.get(base_url)
-        elements = driver.find_elements(By.CSS_SELECTOR, title_selector)
+    @DeprecationWarning
+    def selenium_find_all_titles(self, base_url: str, title_selector: str) -> list[str]:
+        """Doesn't work for AMC page as it returns a lot of empty titles. """
+    
+        self.driver.get(base_url)
+        elements = self.driver.find_elements(By.CSS_SELECTOR, title_selector)
         titles = []
         for element in elements:
             titles.append(element.text)
 
         return titles
+
+    def selenium_find_all_detail_pages(self, base_url: str, page_link_selector: str) -> WebElement:
+        self.driver.implicitly_wait(10) # seconds
+        self.driver.get(base_url)
+        self.driver.implicitly_wait(10) # seconds
+        # element = WebDriverWait(self.driver, 10).until(
+        #     EC.element_to_be_clickable((By.CSS_SELECTOR, page_link_selector))
+        # )
+        # return element
+        return self.driver.find_element(by=By.CSS_SELECTOR, value=page_link_selector)
+
+    def selenium_find_title_from_detail_page(self, pageElement: WebElement) -> str:
+        pageElement.click()
+        element = self.driver.find_element(By.CSS_SELECTOR, AMC_DETAILS_PAGE_TITLE_SELECTOR)
+        return element.text
 
     @classmethod
     def __find_all_titles(cls, requestor: Requestor, base_url: str, title_selector: str) -> list[str]:
--- movie/tests/test_finder.py
+++ movie/tests/test_finder.py
@@ -1,6 +1,9 @@
+import os, sys
 from unittest import TestCase
 
-from movie.finder import TitleFinder, AMC_TITLE_SELECTOR, AMC_INTERNATIONAL_FILM_URL
+from movie.finder import AMC_DETAILS_PAGE_LINK_SELECTOR, TitleFinder, AMC_TITLE_SELECTOR, AMC_INTERNATIONAL_FILM_URL
+
+WEB_DRIVER_PATH = os.getenv("WEB_DRIVER_PATH")
 
 
 class TestFinder(TestCase):
@@ -10,8 +13,22 @@ class TestFinder(TestCase):
         print(titles)
 
     def test_selenium_find_all_titles(self):
-        titles = TitleFinder.selenium_find_all_titles(base_url=AMC_INTERNATIONAL_FILM_URL, title_selector=AMC_TITLE_SELECTOR)
-        assert len(titles) > 0
-        print(titles)
+        finder = TitleFinder()
+        page = finder.selenium_find_all_detail_pages(base_url=AMC_INTERNATIONAL_FILM_URL, page_link_selector=AMC_DETAILS_PAGE_LINK_SELECTOR)
+        title = finder.selenium_find_title_from_detail_page(page)
+        print(title)
+        # assert len(pages) > 0
+        # for page in pages:
+        #     title = finder.selenium_find_title_from_detail_page(page)
+        #     print(title)
+
+    def test_selenium_find_all_names(self):
+        names = TitleFinder.selenium_find_all_titles(base_url="https://www.medicare.gov/care-compare/", title_selector=".ProviderSearchMenuListing__label")
+        print(names)
+
+    def test_chrome_in_path(self):
+        sys.path.append(WEB_DRIVER_PATH)
+        print(sys.path)
+        
 
 
